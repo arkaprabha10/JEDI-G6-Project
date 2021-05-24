3451a4aba5a8efb1fa534cc8b9d065a3ad9d7246
Arkaprbha Banerjee <44334390+arkaprabha10@users.noreply.github.com>
2021-05-24 21:01:10 +0530

    basic structure

---
+++ CRS-JEDI-Flipkart-POS/src/com/flipkart/bean/RegisteredCourses.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.flipkart.bean;
+
+import java.util.ArrayList;
+
+/**
+ * @author Dell
+ *
+ */
+public class RegisteredCourses {
+	private int studentID;
+	private int semesterID;
+	ArrayList<String> course_ID = new ArrayList<String>();
+	ArrayList<String> course_name = new ArrayList<String>();
+	
+	
+}

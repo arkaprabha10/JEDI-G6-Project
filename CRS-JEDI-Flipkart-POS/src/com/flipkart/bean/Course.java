9fe6276dbe24d595987dfc8433c4cec572e78a52
Mahaksh Trivedi <201801418@daiict.ac.in>
2021-05-25 01:35:38 +0530

    Getter setter with constructors added

--- CRS-JEDI-Flipkart-POS/src/com/flipkart/bean/Course.java
+++ CRS-JEDI-Flipkart-POS/src/com/flipkart/bean/Course.java
@@ -8,12 +8,59 @@ package com.flipkart.bean;
  *
  */
 public class Course {
-	
 	private String courseID;
 	private String coursename;
 	private String instructorID;
 	private Integer totalSeats;
 	private Integer availableSeats;
+
+	
+	public Course() {
+		this.courseID = "0";
+		this.coursename = "xyz";
+		this.instructorID = "1";
+		this.totalSeats = 0;
+		this.availableSeats = 0;
+	}
+	public Course(String courseID, String coursename, String instructorID, Integer totalSeats, Integer availableSeats) {
+		super();
+		this.courseID = courseID;
+		this.coursename = coursename;
+		this.instructorID = instructorID;
+		this.totalSeats = totalSeats;
+		this.availableSeats = availableSeats;
+	}
+
+	public String getCourseID() {
+		return courseID;
+	}
+	public void setCourseID(String courseID) {
+		this.courseID = courseID;
+	}
+	public String getCoursename() {
+		return coursename;
+	}
+	public void setCoursename(String coursename) {
+		this.coursename = coursename;
+	}
+	public String getInstructorID() {
+		return instructorID;
+	}
+	public void setInstructorID(String instructorID) {
+		this.instructorID = instructorID;
+	}
+	public Integer getTotalSeats() {
+		return totalSeats;
+	}
+	public void setTotalSeats(Integer totalSeats) {
+		this.totalSeats = totalSeats;
+	}
+	public Integer getAvailableSeats() {
+		return availableSeats;
+	}
+	public void setAvailableSeats(Integer availableSeats) {
+		this.availableSeats = availableSeats;
+	}
 	
 	
 	

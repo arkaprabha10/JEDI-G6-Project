a1d955229582cc0adf313f3d4bad5d49d5809f79
Arkaprbha Banerjee <44334390+arkaprabha10@users.noreply.github.com>
2021-05-25 13:43:34 +0530

    feat: add exception classes

--- CRS-JEDI-Flipkart-POS/src/com/flipkart/exception/CourseNotDeletedException.java
+++ CRS-JEDI-Flipkart-POS/src/com/flipkart/exception/CourseNotDeletedException.java
@@ -7,6 +7,34 @@ package com.flipkart.exception;
  * @author Dell
  *
  */
-public class CourseNotDeletedException {
+public class CourseNotDeletedException extends Exception{
+private String courseID;
+	
+	public CourseNotDeletedException() {
+		// TODO Auto-generated constructor stub
+	courseID = "";
+	}
+	
+	/**
+	 * @param courseID
+	 */
+	public CourseNotDeletedException(String courseID) {
+		super();
+		this.courseID = courseID;
+	}
+
+
+	
+	/**
+	 * @return the courseID
+	 */
+	public String getCourseID() {
+		return courseID;
+	}
+	
+	@Override
+	public String getMessage() {
+		return "CourseID: " + courseID + "is not deleted!";
+	}
 
 }
--- CRS-JEDI-Flipkart-POS/src/com/flipkart/exception/CourseNotFoundException.java
+++ CRS-JEDI-Flipkart-POS/src/com/flipkart/exception/CourseNotFoundException.java
@@ -7,6 +7,34 @@ package com.flipkart.exception;
  * @author Dell
  *
  */
-public class CourseNotFoundException {
+public class CourseNotFoundException extends Exception {
+	private String courseID;
+	
+	public CourseNotFoundException() {
+		// TODO Auto-generated constructor stub
+	courseID = "";
+	}
+	
+	/**
+	 * @param courseID
+	 */
+	public CourseNotFoundException(String courseID) {
+		super();
+		this.courseID = courseID;
+	}
+
+
+	
+	/**
+	 * @return the courseID
+	 */
+	public String getCourseID() {
+		return courseID;
+	}
+	
+	@Override
+	public String getMessage() {
+		return "CourseID: " + courseID + "is not present in course catalogue!";
+	}
 
 }

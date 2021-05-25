70dba027699e7f3f25301bf0758e7688fb57ab38
Jayy <47641025+Jayanth49@users.noreply.github.com>
2021-05-25 13:24:13 +0530

    Added User,student Exceptions

---
+++ CRS-JEDI-Flipkart-POS/src/com/flipkart/exception/UserNotFoundException.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.flipkart.exception;
+
+/**
+ * @author Jayanth
+ *
+ */
+public class UserNotFoundException extends Exception {
+	
+	private String UserId;
+
+	/**
+	 * @param userId
+	 */
+	public void setUserId(String userId) {
+		this.UserId = userId;
+	}
+
+	/**
+	 * Message thrown by user
+	 */
+	public String getMessage() {
+		return "No such user exists! Verify UserID and try again ";
+	}
+	
+}

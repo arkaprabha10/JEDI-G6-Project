491602891dbb9eea9feca2e02b017af016aa6204
Jayy <47641025+Jayanth49@users.noreply.github.com>
2021-05-25 09:58:26 +0530

    Update NotificationInterface.java

--- CRS-JEDI-Flipkart-POS/src/com/flipkart/service/NotificationInterface.java
+++ CRS-JEDI-Flipkart-POS/src/com/flipkart/service/NotificationInterface.java
@@ -3,6 +3,7 @@
  */
 package com.flipkart.service;
 
+import com.flipkart.bean.Notification;
 import com.flipkart.bean.Payment;
 
 /**
@@ -15,8 +16,16 @@ public interface NotificationInterface {
 	 * Method to send Notification depending on status:
 	 * @param studentId: student to be notified
 	 * @param Payment: payment (payment mode,amount and paymentStatus)
-	 * @return notification id for the record added in the database
+	 * @return notificationId for the record added in the database
 	 */
-	 public int sendNotification(int studentId,Payment payment);
+	 public int sendNotification(int studentId,Payment payment,Notification notification);
+	 
+	 
+	 /**
+	  * @param studentId : student to be notified
+	  * @param message : message to send
+	  * @return
+	  */
+	 public Notification GenerateNotification(int studentId,int notificationId,String message);
 	
 }

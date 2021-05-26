8f10884a975545c0b466a446deebd4a826cda721
Jayy <47641025+Jayanth49@users.noreply.github.com>
2021-05-27 01:25:21 +0530

    added instances

--- CRS-JEDI-Flipkart-POS/src/com/flipkart/service/AdminOperation.java
+++ CRS-JEDI-Flipkart-POS/src/com/flipkart/service/AdminOperation.java
@@ -3,6 +3,7 @@
  */
 package com.flipkart.service;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -10,7 +11,6 @@ import com.flipkart.dao.AdminDaoInterface;
 import com.flipkart.dao.AdminDaoOperation;
 import com.flipkart.exception.CourseNotFoundException;
 import com.flipkart.exception.FeesPendingException;
-import com.flipkart.exception.GradeNotAddedException;
 import com.flipkart.exception.UserAlreadyInUseException;
 import com.flipkart.exception.UserNotFoundException;
 import com.flipkart.exception.ProfessorNotAddedException;
@@ -18,7 +18,6 @@ import com.flipkart.exception.StudentNotApprovedException;
 
 import com.flipkart.bean.Course;
 import com.flipkart.bean.Professor;
-import com.flipkart.bean.ReportCard;
 import com.flipkart.bean.Student;
 
 /**
@@ -28,40 +27,89 @@ import com.flipkart.bean.Student;
 
 public class AdminOperation implements AdminInterface {
 	
-	AdminDaoOperation ado = new AdminDaoOperation();
+//	AdminDaoOperation ado = new AdminDaoOperation();
+	private static volatile AdminOperation instance = null;
+	
+	private AdminOperation()
+	{
+		
+	}
+	
+	/**
+	 * Method to make AdminOperation Singleton
+	 */
+	public static AdminOperation getInstance()
+	{
+		if(instance == null)
+		{
+			synchronized(AdminOperation.class){
+				instance = new AdminOperation();
+			}
+		}
+		return instance;
+	}
+	
+
+	AdminDaoInterface ado  =AdminDaoOperation.getInstance();
 
 	@Override
 	public void approveStudentRegistration(int studentId,int semesterId) throws FeesPendingException, StudentNotApprovedException {
 		
-		AdminDaoOperation ado1 = new AdminDaoOperation();
-		ado1.approveStudentRegistration(studentId,semesterId);
+//		AdminDaoOperation ado1 = new AdminDaoOperation();
+		try {
+			ado.approveStudentRegistration(studentId,semesterId);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (FeesPendingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (StudentNotApprovedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 
 	@Override
 	public void addProfessor(Professor professor) {
 		
-		ado.addProfessor(professor);
+		try {
+			ado.addProfessor(professor);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 
 	@Override
 	public void removeProfessor(int professorID) {
 		// TODO Auto-generated method stub
-		ado.removeProfessor(professorID);
+		try {
+			ado.removeProfessor(professorID);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 
 	@Override
-	public ReportCard generateReportCard(int studentID) throws StudentNotApprovedException, GradeNotAddedException, FeesPendingException {
+	public void generateReportCard(int studentID) {
 		// TODO Auto-generated method stub
-		return ado.generateReportCard(studentID);
+		
 	}
 
 	@Override
 	public void removeCourse(int courseID) {
 		// TODO Auto-generated method stub
-		ado.removeCourse(courseID);
+		try {
+			ado.removeCourse(courseID);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 
@@ -74,7 +122,12 @@ public class AdminOperation implements AdminInterface {
 		newCourse.setOfferedSemester(semester);
 		newCourse.setAvailableSeats(10);
 		
-		ado.addCourse(newCourse);
+		try {
+			ado.addCourse(newCourse);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 
@@ -82,7 +135,7 @@ public class AdminOperation implements AdminInterface {
 	@Override
 	public HashMap<String,ArrayList<Integer> > viewCourseStudentList(String courseID, int semester, Boolean viewAll) {
 		
-		AdminDaoOperation ado1 = new AdminDaoOperation();
+//		AdminDaoOperation ado1 = new AdminDaoOperation();
 		return ado1.viewCourseStudentList(courseID,semester,viewAll);
 	}
 
